008f44ea21a7f3e201790cb523fd8c9d19a761a3
Randy Madrigal <randymadrigalx12@gmail.com>
2023-04-11 22:13:38 -0400

    Modify login

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const adminController = require("../controllers/admin");
 
 router.get("/index", adminController.getIndex); //home admin
-router.post("/index", adminController.postFilterIndex); //home admin
+router.post("/index", adminController.postFilterIndex); //home admin with filters employees by name
 
 router.get("/add-employee", adminController.getAddEmployee); //get add-employee
 router.post("/add-employee", adminController.postEmployee); //Post add-employee
--- util/database/database.js
+++ util/database/database.js
@@ -1,16 +1,15 @@
 const Sequelize = require("sequelize");
 const path = require("path"); //para poder accesar a la ruta donde sqlite guarda los archicos (se debe crear una carpeta y un archivo)
 
-/*
 //MySql
 const sequelize = new Sequelize("staffdb", "root", "123456", {
   host: "localhost",
   dialect: "mysql",
   port: 3306,
 });
-*/
 
 //SQLITE
+/*
 const sequelize = new Sequelize("sqlite::memory", {
   dialect: "sqlite",
   storage: path.join(
@@ -18,6 +17,8 @@ const sequelize = new Sequelize("sqlite::memory", {
     "util/database",
     "staffdb.sqlite"
   ),
-});
+
+}
+*/
 
 module.exports = sequelize;
